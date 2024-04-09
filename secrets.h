ffe83138d2bd4bb82a77a3dbb798340e6c60c3c4
Nish245 <162939987+Nish245@users.noreply.github.com>
2024-04-09 19:50:20 +1000

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Nishant"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "12345678"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID   2501768			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "WY2D7J7JK5OB30CJ"   // replace XYZ with your channel write API Key
